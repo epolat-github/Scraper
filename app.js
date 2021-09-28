9ea0d6e39eac32b0d791efc6064e59c865372383
epolat-github <etuna1999@gmail.com>
2021-09-28 11:26:48 +0300

    upgrade outdated packages

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-const PORT = 9000;
+const PORT = 3000;
 
 var app = express();
 
